8dcf612661b567233a17560a50702d1f29e3a7fa
musheabdulhakim <musheabdulhakim99@gmail.com>
2026-07-19 17:42:13 +0000

    fix: staff activity list page

--- app/Filament/Clusters/StaffCluster/Resources/Staff/Pages/ListStaffActivities.php
+++ app/Filament/Clusters/StaffCluster/Resources/Staff/Pages/ListStaffActivities.php
@@ -3,9 +3,9 @@
 namespace Modules\Staff\Filament\Clusters\StaffCluster\Resources\Staff\Pages;
 
 use Modules\Staff\Filament\Clusters\StaffCluster\Resources\Staff\StaffResource;
-use pxlrbt\FilamentActivityLog\Pages\ListActivities;
+use pxlrbt\FilamentActivityLog\Pages\ListActivitiesBySubject;
 
-class ListStaffActivities extends ListActivities
+class ListStaffActivities extends ListActivitiesBySubject
 {
     protected static string $resource = StaffResource::class;
 }
