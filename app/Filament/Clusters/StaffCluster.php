e003d25d932b2a88fcd7af795b5716e60ff3208e
musheabdulhakim <musheabdulhakim99@gmail.com>
2026-09-19 19:36:52 +0000

    feat: place the staff cluster in the Operations sidebar group

--- app/Filament/Clusters/StaffCluster.php
+++ app/Filament/Clusters/StaffCluster.php
@@ -4,12 +4,17 @@ namespace Modules\Staff\Filament\Clusters;
 
 use Filament\Clusters\Cluster;
 use Filament\Support\Icons\Heroicon;
+use Modules\Core\Enums\SidebarGroup;
 
 class StaffCluster extends Cluster
 {
     protected static ?string $slug = 'staff-cluster';
 
     protected static string|\BackedEnum|null $navigationIcon = Heroicon::OutlinedUsers;
+
+    protected static string|\UnitEnum|null $navigationGroup = SidebarGroup::Operations;
+
+    protected static ?int $navigationSort = 20;
 
     protected static bool $shouldRegisterSubNavigation = false;
 }
